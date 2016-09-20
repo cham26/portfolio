0096e547444d32564d276ced5c646a7d1a889035
charmaine ayuban <charmaine ayuban>
2016-09-20 20:22:39 -0300

    profpic image change

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -16,8 +16,25 @@ export class MainController {
       {source: '/assets/images/skills/git.png', id: 'GIT', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dicta fugit perspiciatis? Accusantium cum et id libero nemo quia, similique. Eligendi, expedita, nihil. Animi, aperiam, atque. Mollitia provident quo quod?'},
       {source: '/assets/images/skills/bootstrap.png', id: 'BOOTSTRAP', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dicta fugit perspiciatis? Accusantium cum et id libero nemo quia, similique. Eligendi, expedita, nihil. Animi, aperiam, atque. Mollitia provident quo quod?'},
       {source: '/assets/images/skills/heroku.png', id: 'HEROKU', caption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti dicta fugit perspiciatis? Accusantium cum et id libero nemo quia, similique. Eligendi, expedita, nihil. Animi, aperiam, atque. Mollitia provident quo quod?'}
-    ]
+    ];
+
+    this.profPicImage = ["\assets\images\bio\profPic.jpg", "\assets\images\bio\nerd.jpg", "\assets\images\bio\sideview.jpg"];
+    this.profPicNumber = 0;
+    this.profPicLength = this.profPicImage.profPicLength - 1;
   }
+
+  changeImage(num){
+    this.profPicNumber = this.profPicNumber + num;
+    if (this.profPicNumber > this.profPicLength){
+      this.profPicNumber = 0;
+    }
+    if (this.profPicNumber < 0){
+      this.profPicNumber = this.profPicLength;
+    }
+    document.profPicSlide.src= this.profPicImage[this.profPicNumber];
+    return false;
+  }
+
 }
 
 export default angular.module('portfolioApp.main', [uiRouter])
